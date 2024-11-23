9b3c0909ddacd621a9216e6e1f80336bd1dc144a
OmegaTroy <juli.mjoi44@gmail.com>
2024-11-23 16:23:17 -0300

    add

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import vercel from '@astrojs/vercel/serverless';
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  adapter: vercel()
+  output: "static",
+  adapter: vercel(),
 });
