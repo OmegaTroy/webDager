7674cf807315d273bc7f840b81958701e6e5c7ec
OmegaTroy <juli.mjoi44@gmail.com>
2024-11-23 16:20:19 -0300

    subida de la web

---
+++ tailwind.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        palette1: "#1b1b1b",
+        palette2: "#555151",
+        palette3: "#9f9689",
+        palette4: "#623f2b",
+        palette5: "#b76e2d",
+        palette6: "#fff1e2",
+      },
+    },
+  },
+  plugins: [],
+};
